569ecfb60533186c33f7a196da3777fa8c4989ef
Quentin Aubert <quentino.vega@gmail.com>
2017-11-22 19:50:08 +0100

    Delete config.js

--- src/core/firebase/config.js
+++
@@ -1,7 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyCFHRxg5XytVUyyX24LdWm9sEZk84AReBQ",
-    authDomain: "bdteque.firebaseapp.com",
-    databaseURL: "https://bdteque.firebaseio.com",
-    storageBucket: "bdteque.appspot.com",
-    messagingSenderId: "912080775581"
-};
